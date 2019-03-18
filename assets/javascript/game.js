6156c8d8bbb4b8d5d9d614e80f37e5714368af60
Jonathan Viera <jonathan@Jonathans-MBP.socal.rr.com>
2019-03-17 18:01:28 -0700

    Finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,31 +6,57 @@ var letterBank =
 var wins = 0;
 var losses = 0;
 var guessesLeft = 10;
-var userGuess = [];
+var userGuessChoices = [];
 
 
-var winsText = document.getElementById("win-counter");
-var lossesText = document.getElementById("loss-counter");
-var guessesLeftText = document.getElementById("guess-counter");
-var lettersGuessedText = document.getElementById("letters-guessed");
+var compGuess = letterBank[Math.floor(Math.random() * letterBank.length)];
+
+
+
+document.querySelector("#win-counter").innerHTML = "Wins: " + wins;
+    
+document.querySelector("#loss-counter").innerHTML = "Losses: " + losses;
+
+function updateGuessesLeft() {
+        document.querySelector("#guess-counter").innerHTML = "Guesses left: " + guessesLeft;
+    }
+function guessesSoFar() {
+        document.querySelector("#letters-guessed").innerHTML = "Your guesses so far: " + userGuessChoices.join(" ");
+    }
+    
+updateGuessesLeft();
+guessesSoFar();
+
+var reset = function () {
+    var compGuess = letterBank[Math.floor(Math.random() * letterBank.length)];
+    guessesLeft = 10;
+    userGuessChoices = [];
+}
+
 
 document.onkeyup = function (event) {
-    var userGuess = event.key;
 
-    var compGuess = letterBank[Math.floor(Math.random()*letterBank.length)];
+    guessesLeft--;
 
-    if (compGuess === userGuess) {
-        wins++
-        guessesLeft--
-        (wins);
-    } else if (compGuess != userGuess) {
-        guessesLeft--
-        losses++
-        (losses);
-        
-        
-    }
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    userGuessChoices.push(userGuess);
+
+    updateGuessesLeft();
+    guessesSoFar();
+
+    console.log(compGuess);
 
 
+        if (userGuess === compGuess) {
+            wins++;
+            document.querySelector("#win-counter").innerHTML = "Wins: " + wins;
+            reset();
+            
+        } else if (guessesLeft === 0) {
+            losses++;
+            document.querySelector("#loss-counter").innerHTML = "Losses: " + losses;
+            reset();
+        }
 }
 
